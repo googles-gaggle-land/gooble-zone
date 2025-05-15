bbf907a56b43fd8376be2139aecfed009ef34a03
azloserbits <ribbun.draws@gmail.com>
2025-05-15 17:36:48 +0800

    Revert "Revert "Station records + Few Resprites""

--- Content.Shared/Humanoid/SharedHumanoidAppearanceSystem.cs
+++ Content.Shared/Humanoid/SharedHumanoidAppearanceSystem.cs
@@ -183,6 +183,7 @@ public abstract class SharedHumanoidAppearanceSystem : EntitySystem
         SetSex(target, sourceHumanoid.Sex, false, targetHumanoid);
         targetHumanoid.CustomBaseLayers = new(sourceHumanoid.CustomBaseLayers);
         targetHumanoid.MarkingSet = new(sourceHumanoid.MarkingSet);
+                targetHumanoid.Height = sourceHumanoid.Height; // CD - Character Records
 
         targetHumanoid.Gender = sourceHumanoid.Gender;
         if (TryComp<GrammarComponent>(target, out var grammar))
@@ -445,6 +446,7 @@ public abstract class SharedHumanoidAppearanceSystem : EntitySystem
         }
 
         humanoid.Age = profile.Age;
+        humanoid.Height = profile.Height; // CD - Character Records
 
         RaiseLocalEvent(uid, new ProfileLoadFinishedEvent()); // Shitmed Change
         Dirty(uid, humanoid);
